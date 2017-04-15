902c3bff102eb28f2c845112b92afa5905839ce0
Masquerade <masquerade0097@gmail.com>
2017-04-15 13:55:12 +0530

    first commit omg

--- app/src/main/java/com/example/cyberkillers/digibrary/LoginActivity.java
+++ app/src/main/java/com/example/cyberkillers/digibrary/LoginActivity.java
@@ -33,7 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
-
+//this the first commit
 /**
  * A login screen that offers login via email/password.
  */
